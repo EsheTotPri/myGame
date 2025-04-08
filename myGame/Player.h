d32c7b0e9ffbd0078b2a94f6341f8cbf5cb46ad3
DisraeliOG <118463303+DisraeliOG@users.noreply.github.com>
2025-04-08 08:07:41 +0300

    Update Player.h
    bullet update

--- myGame/Player.h
+++ myGame/Player.h
@@ -1,26 +1,30 @@
-//
-// Created by User on 13.02.2025.
-//
-
 #ifndef PLAYER_H
 #define PLAYER_H
+
+#include "Bullet.h"
+#include "Enemy.h"
 
 #include <SFML/Graphics.hpp>
 
 class Player {
-    private:
-    sf::RectangleShape shape;
+private:
+    sf::Texture playerTexture;
+    sf::Sprite playerSprite;
+    std::vector<Bullet> bullets;
+    sf::Clock shootClock;
 
     float speed;
+    float shootDelay = 0.8f;
 
-    public:
+public:
     Player();
     void handleInput();
-    void update();
+    void update(Enemy& enemy);
     void draw(sf::RenderWindow &window);
 
     sf::Vector2f getPosition() const;
-
+    const std::vector<Bullet>& getBullets() const;
+    std::vector<Bullet>& getBullets();
 };
 
 #endif //PLAYER_H
