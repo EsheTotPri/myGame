9cc8b6a56e50e9da66df1aec38fc841917ee0db2
DisraeliOG <118463303+DisraeliOG@users.noreply.github.com>
2025-04-08 08:09:59 +0300

    Update Enemy.h bullets update added hitbox

--- myGame/Enemy.h
+++ myGame/Enemy.h
@@ -1,34 +1,29 @@
-
-
 #ifndef ENEMY_H
 #define ENEMY_H
 #include <SFML/Graphics/RenderWindow.hpp>
 #include <SFML/Graphics/Sprite.hpp>
 #include <SFML/Graphics/Texture.hpp>
-#include "ctime"
-#include "cstdlib"
 
 class Enemy {
-    private:
-
-    sf:: Sprite enemySprite;
-    sf:: Texture enemyTexture;
+private:
+    sf::Sprite enemySprite;
+    sf::Texture enemyTexture;
     float speed;
     sf::Vector2f direction;
     sf::Clock movementClock;
     float changeDirection;
+    int health;
 
-    public:
-
+public:
     Enemy();
     void update();
-    sf::Vector2f getPosition();
-    void draw(sf::RenderWindow& window) const;
     sf::Vector2f getPosition() const;
+    void draw(sf::RenderWindow& window) const;
     void randomizeDirection();
 
-
+    void takeDamage();
+    bool isAlive() const;
+    sf::FloatRect getBounds() const;
 };
-
 
 #endif //ENEMY_H
