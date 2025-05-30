6ccaf95580cfca24ccfda3530730be299b3b0370
EsheTotPri <eshetotpridurok@gmail.com>
2025-05-30 18:00:20 +0300

    Update Player.h

--- myGame/Player.h
+++ myGame/Player.h
@@ -11,6 +11,7 @@ private:
     sf::Sprite playerSprite;
     std::vector<Bullet> bullets;
     sf::Clock shootClock;
+    int maxHealth = 100;
     int health = 100;
     int experience = 0;
     int level = 1;
@@ -19,10 +20,18 @@ private:
     float shootDelay = 0.8f;
     bool dead = false;
 
+
+    bool justLeveledUp = false;
+    float hpRegen = 0.f;
+    int vampirismHeal = 0;
+    float xpGainMultiplier = 1.f;
+    sf::Clock hpRegenClock;
+
 public:
     Player();
     void update(std::vector<Enemy>& enemies, float deltaTime);
     void draw(sf::RenderWindow &window);
+    void reset();
 
     sf::Vector2f getPosition() const;
     const std::vector<Bullet>& getBullets() const;
@@ -34,11 +43,20 @@ public:
     bool isDead() const { return dead; }
     sf::FloatRect getGlobalBounds() const;
 
-    // Опыт
     void addExperience(int amount);
     int getExperience() const { return experience; }
     int getExpToNextLevel() const { return expToNextLevel; }
     int getLevel() const { return level; }
+
+    //Апгрейды
+    void increaseMaxHealth(float factor);
+    void decreaseShootDelay(float factor);
+    void increaseSpeed(float factor);
+    void enableHpRegen(float amount);
+    void enableVampirism(int heal);
+    void increaseXPGainMultiplier(float factor);
+
+    bool hasJustLeveledUp();
 };
 
 #endif //PLAYER_H
