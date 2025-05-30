942d4411d6d87e3cd3012f14fe3c86b6a2c642f8
EsheTotPri <eshetotpridurok@gmail.com>
2025-05-30 18:00:48 +0300

    Update Player.cpp

--- myGame/Player.cpp
+++ myGame/Player.cpp
@@ -3,6 +3,7 @@
 #include <algorithm>
 #include <cmath>
 #include <iostream>
+#include "Upgrade.h"
 
 Player::Player() {
     if (!playerTexture.loadFromFile("assets/player.png")) {
@@ -22,6 +23,7 @@ void Player::update(std::vector<Enemy>& enemies, float deltaTime) {
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) playerSprite.move(0, speed * deltaTime);
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) playerSprite.move(-speed * deltaTime, 0);
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) playerSprite.move(speed * deltaTime, 0);
+
 
     Enemy* closestEnemy = nullptr;
     float closestDistanceSq = std::numeric_limits<float>::max();
@@ -58,6 +60,11 @@ void Player::update(std::vector<Enemy>& enemies, float deltaTime) {
             if (enemy.isAlive() && bullet.isActive && bullet.getBounds().intersects(enemy.getBounds())) {
                 bullet.isActive = false;
                 enemy.takeDamage();
+
+                if (!enemy.isAlive() && vampirismHeal > 0) {
+                    health = std::min(maxHealth, health + vampirismHeal);
+                }
+
                 break;
             }
         }
@@ -66,6 +73,12 @@ void Player::update(std::vector<Enemy>& enemies, float deltaTime) {
     bullets.erase(std::remove_if(bullets.begin(), bullets.end(),
                                  [](const Bullet& b) { return !b.isActive; }),
                   bullets.end());
+
+    if (hpRegen > 0.f && hpRegenClock.getElapsedTime().asSeconds() >= 1.f) {
+        health = std::min(maxHealth, health + static_cast<int>(hpRegen));
+        hpRegenClock.restart();
+    }
+
 }
 
 void Player::draw(sf::RenderWindow &window) {
@@ -98,11 +111,65 @@ std::vector<Bullet>& Player::getBullets() {
 }
 
 void Player::addExperience(int amount) {
+    amount = static_cast<int>(amount * xpGainMultiplier);
     experience += amount;
     while (experience >= expToNextLevel) {
         experience -= expToNextLevel;
         level++;
-        expToNextLevel = static_cast<int>(expToNextLevel * 1.5f); // На след лвл нужно больше опыта
+        expToNextLevel = static_cast<int>(expToNextLevel * 1.5f);
+        justLeveledUp = true;
         std::cout << "Level up! Now level " << level << std::endl;
     }
+}
+
+void Player::reset() {
+    if (!playerTexture.loadFromFile("assets/player.png")) {
+        std::cout << "Error loading player texture" << std::endl;
+    }
+    playerSprite.setTexture(playerTexture);
+    sf::FloatRect bounds = playerSprite.getLocalBounds();
+    playerSprite.setOrigin(bounds.width / 2, bounds.height / 2);
+    playerSprite.setPosition({400, 300});
+    playerSprite.setScale(0.15f, 0.15f);
+
+    health = maxHealth = 100;
+    maxHealth = 100;
+    experience = 0;
+    level = 1;
+    expToNextLevel = 100;
+    speed = 200.f;
+    dead = false;
+    bullets.clear();
+    shootClock.restart();
+}
+
+void Player::increaseMaxHealth(float factor) {
+    maxHealth = static_cast<int>(maxHealth * factor);
+    health = std::min(health, maxHealth);
+}
+
+void Player::decreaseShootDelay(float factor) {
+    shootDelay *= factor;
+}
+
+void Player::increaseSpeed(float factor) {
+    speed *= factor;
+}
+
+bool Player::hasJustLeveledUp() {
+    bool temp = justLeveledUp;
+    justLeveledUp = false;
+    return temp;
+}
+
+void Player::enableHpRegen(float amount) {
+    hpRegen += amount;
+}
+
+void Player::enableVampirism(int heal) {
+    vampirismHeal += heal;
+}
+
+void Player::increaseXPGainMultiplier(float factor) {
+    xpGainMultiplier *= factor;
 }
